8e5b4da39581a83606e2f46798a9c714dd9b7546
Dkrot-tech <danakrotovic@gmail.com>
2026-04-06 05:04:50 +0300

    Первый коммит

---
+++ der.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <string>
+#include <algorithm>
+#include <cctype>
+
+int main() {
+    std::string text;
+    
+    std::cout << "Enter string: ";
+    std::getline(std::cin, text);
+    
+    // Удаляем все знаки препинания и служебные символы, кроме пробела
+    text.erase(std::remove_if(text.begin(), text.end(), [](char c) {
+        return std::ispunct(static_cast<unsigned char>(c)) || 
+               (std::iscntrl(static_cast<unsigned char>(c)) && c != ' ');
+    }), text.end());
+    
+    std::cout << "Result: " << text << std::endl;
+    
+    return 0;
+}
