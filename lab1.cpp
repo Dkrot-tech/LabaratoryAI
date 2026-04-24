16bd14ed7197a3a2de4660883c2377a66f193e85
abakool <vladimir.abakulov@gmail.com>
2026-04-24 09:16:05 +0700

    jjjjj

---
+++ lab1.cpp
@@ -0,0 +1,48 @@
+#include <iostream>
+#include <string>
+#include <cctype> 
+
+using namespace std;
+
+
+bool isPalindrome(const string& str) {
+    int left = 0;                    
+    int right = str.length() - 1;   
+    
+    while (left < right) {
+        
+        while (left < right && !isalnum(str[left])) {
+            left++;
+        }
+        while (left < right && !isalnum(str[right])) {
+            right--;
+        }
+        
+        
+        if (tolower(str[left]) != tolower(str[right])) {
+            return false; 
+        }
+        
+        left++;  
+        right--; 
+    }
+    
+    return true;
+}
+
+int main() {
+    string input;
+    
+    cout << "Введите строку для проверки: ";
+    getline(cin, input); 
+    
+    
+    if (isPalindrome(input)) {
+        cout << "Да, строка является палиндромом!" << endl;
+    } else {
+        cout << "Нет, строка не является палиндромом." << endl;
+    }
+    
+    return 0;
+}
+
